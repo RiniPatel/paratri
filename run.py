a61b44f8345382b8d53913c2093a63b20f39355a
Tushar Goyal <goyaltushar@cmu.edu>
2018-04-18 14:35:51 -0400

    Adding auto mode to the runner

--- run.py
+++ run.py
@@ -7,11 +7,30 @@ import subprocess
 BINARY_NAME = "./triangle"
 BECNHMARK_TYPE_SUFFIX = "_adj"
 
-local_benchmarks = ["amazon0302"]
+local_benchmarks = [
+"oregon1_010526"
+]
 
 parser = argparse.ArgumentParser(description='Runner for triangle benchmarks')
 parser.add_argument('-i', '--input', help = "Input CSV file", dest = 'csv_file', required = True)
 parser.add_argument('-b', '--benchmark-dir', help = "benchmarks directory", dest = 'benchmark_dir', required = True)
+parser.add_argument('-m', '--mode', help = "Mode auto/manual", dest = 'mode', required = True)
+
+total_count = 0
+
+def run_auto(benchmark, num_nodes, num_edges, dataset):
+    global total_count
+    if os.path.isfile(dataset + "_IA.txt") and os.path.isfile(dataset + "_JA.txt"):
+        print "Running " + benchmark['dataset']
+        subprocess.call([BINARY_NAME, dataset, num_nodes, num_edges])
+        total_count += 1
+
+def run_manual(benchmark, num_nodes, num_edges, dataset):
+    global total_count
+    if benchmark['dataset'] in local_benchmarks:
+        print "Running " + benchmark['dataset']
+        subprocess.call([BINARY_NAME, dataset, num_nodes, num_edges])
+        total_count += 1
 
 def main():
     args = parser.parse_args()
@@ -27,9 +46,13 @@ def main():
             num_edges = benchmark['num_edges']
             dataset = args.benchmark_dir + benchmark['dataset'] + BECNHMARK_TYPE_SUFFIX
 
-            if benchmark['dataset'] in local_benchmarks:
-                print "Running " + benchmark['dataset']
-                subprocess.call([BINARY_NAME, dataset, num_nodes, num_edges])
+            if args.mode == "manual":
+                run_manual(benchmark, num_nodes, num_edges, dataset)
+            else:
+                run_auto(benchmark, num_nodes, num_edges, dataset)
+
+    print "Total benchmarks = ", total_count
+                
             
 
 if __name__ == "__main__":
