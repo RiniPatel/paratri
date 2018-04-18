f4f374f09833846a0a31b34b1748c8bd1cec576d
Rini Patel <rinip@andrew.cmu.edu>
2018-04-18 13:08:47 -0400

    adding script to convert MMIO to CSR

--- graphs/read-mmio.py
+++ graphs/read-mmio.py
@@ -3,8 +3,13 @@
 from scipy.io import mmread
 import numpy
 
-adj = mmread("amazon0302_adj.mmio")
-# print type(adj)
-csr = adj.tocsr()
-numpy.savetxt("amazon0302_adj_IA.txt", csr.indptr, fmt = '%d')
-numpy.savetxt("amazon0302_adj_JA.txt", csr.indices, fmt = '%d')
+import os
+for file in os.listdir("./"):
+    if file.endswith(".mmio"):
+        adj = mmread(file)
+        csr = adj.tocsr()
+        file = os.path.splitext(file)[0]
+        print file
+        numpy.savetxt(file + "_IA.txt", csr.indptr, fmt = '%d')
+        numpy.savetxt(file + "_JA.txt", csr.indices, fmt = '%d')
+        print "Generated " + file + "_IA.txt and " + file + "_JA.txt"
